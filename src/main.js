45aa54315eb87d7e76940ee03b15b07fafa0af60
alex-twan-duong <alex.duong@clearroute.io>
2024-10-13 22:00:33 +0000

    Add logic

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core')
 const { wait } = require('./wait')
+const github = require('@actions/github')
 
 /**
  * The main function for the action.
@@ -7,18 +8,72 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    //
+    const owner = core.getInput('owner', { required: true })
+    const repo = core.getInput('repo', { required: true })
+    const pr_no = core.getInput('pr_no', { required: true })
+    const token = core.getInput('token', { required: true })
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    //init octokit
+    const octokit = github.getOctokit(token)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const { data: changedFiles } = await octokit.rest.pulls.listFiles({
+      owner,
+      repo,
+      pull_number: pr_no
+    })
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    let diffData = {
+      addition: 0,
+      deletion: 0,
+      changes: 0
+    }
+
+    diffData = changedFiles.reduce((acc, file) => {
+      acc.additions += file.additions
+      acc.deletions += file.deletions
+      acc.changes += file.changes
+      return acc
+    }, diffData)
+
+    //Create comments
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      pr_no,
+      body: `
+        PR #${pr_no} update with \n
+        - ${diffData.changes} changes \n
+        - ${diffData.addition} additions \n
+      `
+    })
+
+    //Add Label
+    for (const file of changedFiles) {
+      const fileExtension = file.filename.split('.').pop()
+
+      let label = ''
+      switch (fileExtension) {
+        case 'md':
+          label = 'Markdown'
+          break
+        case 'js':
+          label = 'JavaScript'
+          break
+        case 'yml':
+          label = 'Yaml'
+          break
+        default:
+          label = 'no extension'
+      }
+      // set Labels
+      await octokit.rest.issues.addLabels({
+        owner,
+        repo,
+        pr_no,
+        labels: [label]
+      })
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
